bb5758e25bfeb9ccc01ff0623f947d3380e7edab
Lisa Marie <elisa@funkykitty.net>
2015-04-12 21:34:25 +0000

    Moved follow code to follow.c/h

---
+++ src/party/follow.h
@@ -0,0 +1,6 @@
+#ifndef _FOLLOW_H
+#define _FOLLOW_H
+
+void init_follow(void);
+
+#endif
--- src/party/module.mk
+++ src/party/module.mk
@@ -1,5 +1,7 @@
 # include our source code
+SRC += party/follow.c
 SRC += party/party.c
+
 # include our required libraries
 #LIBS += -labc
 # and finally, our compiler flags
