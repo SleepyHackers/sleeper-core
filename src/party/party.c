a100c2f38351f82fc99a838fd600f5a744293c25
Lisa Marie <elisa@funkykitty.net>
2015-04-12 15:26:01 +0000

    Refactored follow comand. Added ability to unfollow.

--- src/party/party.c
+++ src/party/party.c
@@ -6,7 +6,7 @@
 
 #include "party.h"
 
-LIST *leaders = NULL;
+LIST *leaders = NULL, *followers;
 
 /* leader comparator for list* functions */
 int leaderFind(const CHAR_DATA *cmpto, const LEADER_DATA *cur) {
@@ -14,8 +14,12 @@ int leaderFind(const CHAR_DATA *cmpto, const LEADER_DATA *cur) {
 }
 
 /* follower comparator for list* functions */
-int followerFind(const CHAR_DATA *cmpto, const LEADER_DATA *cur) {
+int followerListFind(const CHAR_DATA *cmpto, const LEADER_DATA *cur) {
   return cur != cmpto;
+}
+
+int followerFind(const CHAR_DATA *cmpto, const FOLLOWER_DATA *cur) {
+  return cur->follower != cmpto;
 }
 
 /* Creates a new leader, with a first follower */
@@ -39,6 +43,23 @@ void deleteLeader(LEADER_DATA *ldLeader) {
   free(ldLeader);
 }
 
+/* Creates a new leader, with a first follower */
+FOLLOWER_DATA *newFollower(CHAR_DATA *cdLeader, CHAR_DATA *cdFollower) {
+  FOLLOWER_DATA *fdNew = malloc(sizeof(FOLLOWER_DATA));
+  
+  /* Set the leader, create a new list of followers, add the follower */
+  fdNew->leader = cdLeader;
+  fdNew->follower = cdFollower;
+
+  return fdNew; /* Return the new struct */
+}
+
+void deleteFollower(FOLLOWER_DATA *ldFollower) {
+  /* cleanup */
+  free(ldFollower);
+}
+
+
 /* Hook to run on "enter", allows following of NPCs and PCs */
 void doFollow(char *info) {
   char buffy[MAX_BUFFER];
@@ -59,7 +80,92 @@ void doFollow(char *info) {
   }
 }
 
-/* Follows a target player. Takes one or no args. 
+
+/* Adds char to the follower/leader lists */
+void startFollow(CHAR_DATA *ch, char *arg) {
+  CHAR_DATA *cdTmp = NULL, *cdTmp1 = NULL;
+  FOLLOWER_DATA *fdTmp = NULL;
+  LEADER_DATA *ldTmp = NULL;
+ 
+
+  /* Find the char, if in the room and can see */
+  /*
+    leader = CHAR_DATA *find_char(ch, LIST *list, int num, const char *name,
+				 const char *prototype, bool must_see);
+  */
+
+   cdTmp = find_char(ch, roomGetCharacters(charGetRoom(ch)), 1, arg, NULL, TRUE);
+
+   /* Test to make sure char is not following */
+   fdTmp = listGetWith(followers, ch, followerFind);
+   if(fdTmp == NULL) {
+
+     if(cdTmp == NULL) {  /* Did we fail to find the char? */
+       send_to_char(ch, "There is no %s to follow.\r\n", arg);
+       
+     } else if(cdTmp == ch) { /* Cannot follow yourself */
+       send_to_char(ch, "You cannot follow yourself!\r\n");
+       
+     } else { /* All clear, follow the leader now if possible */
+       
+       ldTmp = listGetWith(leaders, cdTmp, leaderFind);
+       
+       if(ldTmp != NULL) {
+	 cdTmp1 = listGetWith(ldTmp->followers, ch, followerListFind);
+	 
+	 if(cdTmp1 == NULL) {
+	   listPut(ldTmp->followers, ch);
+	   listPut(followers, newFollower(cdTmp, ch));
+
+	   send_to_char(ch, "Now following %s.\r\n", charGetName(cdTmp));
+	 } else {
+	   send_to_char(ch, "You are already following %s.\r\n", charGetName(cdTmp));	
+	 }
+       } else {
+	 listPut(leaders, newLeader(cdTmp, ch));
+	 listPut(followers, newFollower(cdTmp, ch));
+	 send_to_char(ch, "Now following %s.\r\n", charGetName(cdTmp));
+       }
+     }
+   } else {
+     send_to_char(ch, "Already following someone.\r\n", charGetName(fdTmp->leader));
+   }
+}
+
+/* Removes char from the follower/leader lists */
+void stopFollow(CHAR_DATA *ch) {
+  /* Find our leader */
+  CHAR_DATA *cdTmp = NULL, *cdTmp1 = NULL;
+  FOLLOWER_DATA *fdTmp = NULL;
+  LEADER_DATA *ldTmp = NULL;
+
+   /* Test to make sure char is not following */
+   fdTmp = listGetWith(followers, ch, followerFind);
+  
+   if(fdTmp != NULL) {
+     send_to_char(ch, "You no longer follow %s.\r\n", charGetName(fdTmp->leader));
+     /* Delete from leaders followers */
+     ldTmp = listGetWith(leaders, fdTmp->leader, leaderFind);
+     listRemoveWith(ldTmp->followers, ch, followerListFind);
+
+     /* Last follower? Delete leader */
+     if(isListEmpty(ldTmp->followers)) {
+       listRemoveWith(leaders, fdTmp->leader, leaderFind);
+       deleteLeader(ldTmp);
+     }
+
+     /* Delete follower data */
+     listRemoveWith(followers, ch, followerListFind);
+     deleteFollower(fdTmp);
+
+
+   } else {
+     send_to_char(ch, "follow {target}\r\n");
+   }
+  
+}
+
+/* (un)Follows a target player. Takes one or no args. 
    arg1 - The target
    If no args are given, stops following players
 
@@ -70,48 +176,19 @@ COMMAND(cmd_follow) {
   char buffy[MAX_BUFFER];
   char buffy1[MAX_BUFFER];
 
-  CHAR_DATA *cdTmp = NULL, *cdTmp1; /* Temp pointer for leader */
-
-  arg = one_arg(arg, buffy);  /* Get the first arg */  
-
-  /* Find the char, if in the room and can see */
-  /*
-    leader = CHAR_DATA *find_char(ch, LIST *list, int num, const char *name,
-				 const char *prototype, bool must_see);
-  */
-
-   cdTmp = find_char(ch, roomGetCharacters(charGetRoom(ch)), 1, buffy, NULL, TRUE);
-
-   if(cdTmp == NULL) {  /* Did we fail to find the char? */
-     send_to_char(ch, "There is no %s to follow.\r\n", buffy);
-
-   } else if(cdTmp == ch) { /* Cannot follow yourself */
-     send_to_char(ch, "You cannot follow yourself!\r\n");
-
-   } else { /* All clear, follow the leader now if possible */
-
-    LEADER_DATA *ldTmp = NULL;
-    
-    ldTmp = listGetWith(leaders, cdTmp, leaderFind);
-
-    if(ldTmp != NULL) {
-      cdTmp1 = listGetWith(ldTmp->followers, ch, followerFind);
-      if(cdTmp1 == NULL) {
-	listPut(ldTmp->followers, ch);
-	send_to_char(ch, "Now following %s.\r\n", charGetName(cdTmp));
-      } else {
-	send_to_char(ch, "You are already following %s.\r\n", charGetName(cdTmp));	
-      }
-    } else {
-      listPut(leaders, newLeader(cdTmp, ch));
-      send_to_char(ch, "Now following %s.\r\n", charGetName(cdTmp));
-    }
-
+  /* Test for no args */
+  if(compares(arg, "")) {
+    stopFollow(ch);
+  } else {
+    arg = one_arg(arg, buffy);  /* Get the first arg */ 
+    startFollow(ch, buffy);
   }
+
 }
 
 void init_party(void) {
   add_cmd("follow", NULL, cmd_follow, "player", FALSE); /* Add the follow command */
   hookAdd("enter", doFollow);                           /* Add the follow hook to "enter" */
   leaders = newList();                                  /* Init our list of leaders */
+  followers = newList();                                /* Ini tour list of followers */
 }
