f448d38361272d6433d18d71b17d60aafb8898f6
Felix Avalon <zamnedix@sdf.org>
2015-04-27 05:35:49 +0000

    Compiles now, added some comments

--- src/stats/stats.c
+++ src/stats/stats.c
@@ -1,91 +1,92 @@
-nclude "../storage.h"
-#include "../auxiliary.h"
-#include "../world.h"
+#include <stdlib.h> //For malloc
+//CFLAGS in module.mk allows us a nicer syntax here
+#include "mud.h" //Have to include this for pretty much anything
+#include "storage.h"
+#include "auxiliary.h"
+#include "world.h"
 
 
 //Structs for character data
 
 struct char_stats {
-	 int strength;
-    int body
-    int quickness;
-	 int charisma;
-	 int willpower;
-    int intelligence;
-    long money;
-    long bank_balance;
-    int physical_condition;
-    int mental_condition;
-    float essence;
-    float bio_index;
-																													              
-}
+  int strength;
+  int body;
+  int quickness;
+  int charisma;
+  int willpower;
+  int intelligence;
+  long money;
+  long bank_balance;
+  int physical_condition;
+  int mental_condition;
+  float essence;
+  float bio_index;
+}; //Semicolon at end of struct definitions
        
        
-char_stats *newStats () {
-	 char_stats *character = calloc(1, sizeof(char_stats));
+struct char_stats *newStats () { //Struct keyword
+  //Calloc is almost never necessary, a plain malloc will suffice
+  struct char_stats *character = malloc(sizeof(*character)); //Must use struct keyword
 
-    character-->strength  = 1;
-    character-->body      = 1;
-    character-->quickness = 1;
-    character-->charisma  = 1;
-    character-->willpower = 1;
-    character-->intelligence  = 1;
-    character-->money         = 0;
-    character-->bank_balance  = 0;
-    character-->physical_condition    = 10;
-    character-->mental_condition      = 10;
-    character-->essence               = 6;
-    character-->bio_index             = 9;
+  //Pointer access operator is ->
+  character->strength              = 1;
+  character->body                  = 1;
+  character->quickness             = 1;
+  character->charisma              = 1;
+  character->willpower             = 1;
+  character->intelligence          = 1;
+  character->money                 = 0;
+  character->bank_balance          = 0;
+  character->physical_condition    = 10;
+  character->mental_condition      = 10;
+  character->essence               = 6;
+  character->bio_index             = 9;
+  
+  //Return pointer to new character
+  return character;
 }
 
 //to retrieve values
 
-int charGetStrength (char_stats *character) {
-	    return character->strength;
+//Gotta tell the compiler it's a struct here, unless you use a typedef somewhere.
+int charGetStrength (struct char_stats *character) {
+  return character->strength;
 }
 
-int charGetBody (char_stats *character) {
-	    return character->body;
+//charGetBody function name already taken
+int charGetBod (struct char_stats *character) {
+  return character->body;
 }
 
-int charGetQuickness (char_stats *character) {
-	    return character->quickness;
+int charGetQuickness (struct char_stats *character) {
+  return character->quickness;
 }
 
-int charGetCharisma (char_stats *character) {
-	    return character->charisma;
+int charGetCharisma (struct char_stats *character) {
+  return character->charisma;
 }
 
-int charGetWillpower (char_stats *character) {
-	    return character->willpower;
+int charGetWillpower (struct char_stats *character) {
+  return character->willpower;
 }
 
-int charGetIntelligence (char_stats *character) {
-	    return character->intelligence;
+int charGetIntelligence (struct char_stats *character) {
+  return character->intelligence;
 }
 
-long charGetMoney (char_stats *character) {
-	    return character->money;
-}
-    
-long charGetBankBal (char_stats *character) {
-	    return character->bank_balance;
+long charGetBankBal (struct char_stats *character) {
+  return character->bank_balance;
 }       
 
-int charGetMoney (char_stats *character) {
-	    return character->physical_condition;
-}
-
-int charGetMoney (char_stats *character) {
-	    return character->mental_condition;
+long charGetMoney (struct char_stats *character) {
+  return character->money;
 }
        
-float getEssence (char_stats *character) {
-	      character->essence;
+float getEssence (struct char_stats *character) {
+  return character->essence;
 }
 
-float getBioIndex (char_stats *character) {
-	      character->bio_index;
+float getBioIndex (struct char_stats *character) {
+  return character->bio_index;
 }
 
