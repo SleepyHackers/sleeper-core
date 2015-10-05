f6f5a09e468a13f667045c4de6f25cbd455b1aa1
Joshua Drake <zamnedix@sleepermud.net>
2015-10-05 17:44:48 +0000

    Cleanup before rebase

--- src/modules/party/party.c
+++ src/modules/party/party.c
@@ -1,13 +1,13 @@
 #include <python2.7/Python.h>        // to add Python hooks
 
-#include "../mud.h"
-#include "../utils.h"
-#include "../hashtable.h"
-#include "../room.h"
-#include "../exit.h"
-#include "../character.h"
-#include "../list.h"
-#include "../hooks.h"
+#include "mud.h"
+#include "utils.h"
+#include "hashtable.h"
+#include "room.h"
+#include "exit.h"
+#include "character.h"
+#include "list.h"
+#include "hooks.h"
 
 #include "party.h"
 #include "follow.h"
