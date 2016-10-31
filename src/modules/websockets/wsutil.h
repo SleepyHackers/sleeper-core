d47dfabe8358e552ec9e47761a341016f65b2a10
Lisa Marie Maginnis <lisam@gnu.org>
2016-10-31 04:18:44 +0000

    Removed proof of concept websockets module

--- src/modules/websockets/wsutil.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int b64max(int i);
-void b64encode(char* input, char* output, size_t sz);
-void b64decode(char* input, char* output, size_t sz);
--- src/modules/webui_proto/webui_proto.h
+++
@@ -1,7 +0,0 @@
-#ifndef WEBUI_PROTO_H
-#define WEBUI_PROTO_H
-
-void webuiproto_add_cmd(char *cmd, void *func);
-void webuiproto_remove_cmd(char *cmd);
-
-#endif
